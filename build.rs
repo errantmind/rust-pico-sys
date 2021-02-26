dd59be4c70f50f6996c8352b41502d83d4040284
James Bates <jmbates@gmail.com>
2021-02-25 19:12:39 -0600

    test iteration

--- build.rs
+++ build.rs
@@ -6,11 +6,12 @@ fn main() {
     #[allow(deprecated)]
     cc::Build::new()
         .file("extern/picohttpparser/picohttpparser.c")
+        .include("extern/picohttpparser")
         .flag("-funroll-loops")
         .flag("-msse4")
         .flag("-flto")
         .flag("-Ofast")
-        .compile("libpico.a");
+        .compile("libpicohttpparser.a");
     // cc::compile_library(
     //     "libpico.a",
     //     &["extern/picohttpparser/picohttpparser.c"]);
